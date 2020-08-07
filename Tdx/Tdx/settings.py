5e13a0881abb844a90b837cc9da56d06bc297d38
ItsRaza <razaabbas768@gmail.com>
2020-08-08 02:03:56 +0500

    app start

--- Tdx/Tdx/settings.py
+++ Tdx/Tdx/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Points',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,9 @@ WSGI_APPLICATION = 'Tdx.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'Points',
+        'USER': 'Raza',
     }
 }
 
