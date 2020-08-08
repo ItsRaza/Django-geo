a474d3edb21c1ee715818d7d24f0e5c89d5b8f32
ItsRaza <razaabbas768@gmail.com>
2020-08-09 01:13:15 +0500

    redo

--- try.py
+++ try.py
@@ -1,3 +1,8 @@
-import gdal
-print("hello")
-osgeo.gdal.VersionInfo()
+from django.contrib.gis.db import models
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.PointField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
